59ec777c506e0596034975c4909fd4add4be5227
Marcus Neo <marcus121neo@gmail.com>
2024-05-02 18:08:29 +0800

    jpa study initial commit

---
+++ src/main/java/com/marcuscoding/jpa/models/Author.java
@@ -0,0 +1,35 @@
+package com.marcuscoding.jpa.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+//@Table(name = "AUTHOR_TBL")
+public class Author {
+    @Id
+    @GeneratedValue
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_sequence")
+    //@SequenceGenerator(name = "author_sequence", sequenceName = "author_sequence", allocationSize = 1)
+    //this gen-value work with primary key
+    //why use Integer, but not int
+    //because hibernate will check, if got value will use the value
+    //0 should not be the default for ID
+    private Integer id;
+    //    @Column(name = "f_name", length = 35)
+    private String firstName;
+    private String lastName;
+    //    @Column(
+//            unique = true,
+//            nullable = false
+//    )
+    private String email;
+    private int age;
+    //    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    //    @Column(insertable = false)
+    private LocalDateTime lastModified;
+}
